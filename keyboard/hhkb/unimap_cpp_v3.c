3bd14420f121d800e5ff303da4c3676cd65bf179
Ye Feng <feng_ye2001@hotmail.com>
2019-07-28 23:00:49 +1200

    Make minus(-) key, equal(=) key and @ key where it used to be. Reposition quotes and double quotes and grave keys to accommodate.

--- keyboard/hhkb/unimap_cpp_v3.c
+++ keyboard/hhkb/unimap_cpp_v3.c
@@ -9,6 +9,7 @@ enum macro_id {
     L3_LBRACKET,
     L3_RBRACKET,
     L3_BSLASH,
+    L3_GRAVE,
 };
 
 enum function_id {
@@ -46,7 +47,7 @@ enum function_id {
 #define AC_L3(KEY)       ACTION_MODS_KEY(MOD_LSFT, KC_##KEY)
 
 // 1st row
-#define AC_L3_GRAV       ACTION_KEY(KC_GRAVE)
+// #define AC_L3_GRAV       ACTION_KEY(KC_GRAVE)
 #define AC_L3_DBQT       ACTION_MODS_KEY(MOD_LSFT, KC_QUOTE)
 #define AC_L3_SGQT       ACTION_MACRO(L3_QUOTE)
 #define AC_L3_PIPE       ACTION_MODS_KEY(MOD_LSFT, KC_BSLASH)
@@ -56,8 +57,9 @@ enum function_id {
 #define AC_L3_RPRN       ACTION_MODS_KEY(MOD_LSFT, KC_0)
 #define AC_L3_LBKT       ACTION_MACRO(L3_LBRACKET)
 #define AC_L3_RBKT       ACTION_MACRO(L3_RBRACKET)
-#define AC_L3_AT         ACTION_MODS_KEY(MOD_LSFT, KC_2)
+// #define AC_L3_AT         ACTION_MODS_KEY(MOD_LSFT, KC_2)
 #define AC_L3_TIDL       ACTION_MODS_KEY(MOD_LSFT, KC_GRAVE)
+#define AC_L3_GRV        ACTION_MACRO(L3_GRAVE)
 // 2nd row
 #define AC_L3_LSBK       ACTION_MODS_KEY(MOD_LSFT, KC_COMMA)
 #define AC_L3_RSBK       ACTION_MODS_KEY(MOD_LSFT, KC_DOT)
@@ -75,7 +77,7 @@ enum function_id {
 // L4, for remapped R Shift+X keycodes
 #define AC_L4(KEY)       ACTION_MODS_KEY(MOD_RSFT, KC_##KEY)
 // 1st row
-#define AC_L4_GRAV       ACTION_KEY(KC_GRAVE)
+// #define AC_L4_GRAV       ACTION_KEY(KC_GRAVE)
 #define AC_L4_DBQT       ACTION_MODS_KEY(MOD_RSFT, KC_QUOTE)
 #define AC_L4_SGQT       ACTION_MACRO(L3_QUOTE)
 #define AC_L4_PIPE       ACTION_MODS_KEY(MOD_RSFT, KC_BSLASH)
@@ -85,8 +87,9 @@ enum function_id {
 #define AC_L4_RPRN       ACTION_MODS_KEY(MOD_RSFT, KC_0)
 #define AC_L4_LBKT       ACTION_MACRO(L3_LBRACKET)
 #define AC_L4_RBKT       ACTION_MACRO(L3_RBRACKET)
-#define AC_L4_AT         ACTION_MODS_KEY(MOD_RSFT, KC_2)
+// #define AC_L4_AT         ACTION_MODS_KEY(MOD_RSFT, KC_2)
 #define AC_L4_TIDL       ACTION_MODS_KEY(MOD_RSFT, KC_GRAVE)
+#define AC_L4_GRV        ACTION_MACRO(L3_GRAVE)
 // 2nd row
 #define AC_L4_LSBK       ACTION_MODS_KEY(MOD_RSFT, KC_COMMA)
 #define AC_L4_RSBK       ACTION_MODS_KEY(MOD_RSFT, KC_DOT)
@@ -106,7 +109,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] __attribute__ ((section ("
 const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
 #endif
     [0] = UNIMAP_HHKB(
-    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   EQL,    L0_LPRN, L0_RPRN,    GRV,
+    ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS, EQL,L0_LPRN,L0_RPRN,
     TABF,Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   L0_LCBK,L0_RCBK,    BSPC,
     LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,L0_COLN,            ENT,
     FN3L,Z,   X,   C,   V,   B,   N,   M,   COMM, DOT,SLSH,   FN4R,            FN1,
@@ -130,7 +133,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
 
     // Remapped L Shift-X keys
     [3] = UNIMAP_HHKB(
-    L3_AT,  L3(1),  L3_DBQT,L3_SGQT,L3_PIPE,L3(5), L3(6),  L3(7),  L3(8),L3_MINS,L3_PLUS,  L3_USCR,L3_LBKT,L3_RBKT,L3_TIDL,
+    L3_TIDL,L3(1), L3(2), L3_GRV,L3_PIPE,  L3(5),  L3(6),  L3(7),  L3(8),L3_DBQT,L3_SGQT,  L3_PLUS,L3_USCR,L3_LBKT,L3_RBKT,
     TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,  L3_LSBK,L3_RSBK,            DEL,
     TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,L3_HASH,  L3_DLLR,                L3_CCMT,
     TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   L3_QSTM,L3_CPTR,L3_BSLS,          TRNS,           TRNS,
@@ -138,7 +141,7 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
 
     // Remapped R Shift-X keys
     [4] = UNIMAP_HHKB(
-    L4_AT, L4(1),L4_DBQT,L4_SGQT,L4_PIPE,  L4(5),  L4(6),  L4(7),  L4(8),L4_MINS,L4_PLUS,  L4_USCR,L4_LBKT,L4_RBKT,L4_TIDL,
+    L4_TIDL,L4(1), L4(2), L4_GRV,L4_PIPE,  L4(5),  L4(6),  L4(7),  L4(8),L4_DBQT,L4_SGQT,  L4_PLUS,L4_USCR,L4_LBKT,L4_RBKT,
     TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,  L4_LSBK,L4_RSBK,            DEL,
     TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,L4_HASH,  L4_DLLR,                L4_CCMT,
     TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   TRNS,   L4_QSTM,L4_CPTR,L4_BSLS,          TRNS,           TRNS,
@@ -190,6 +193,10 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
         case L3_BSLASH:
             return (record->event.pressed ?
                     MACRO( I(0), SM(), CM(), T(BSLASH), RM(), END ) : 
+                    MACRO_NONE );
+        case L3_GRAVE:
+            return (record->event.pressed ?
+                    MACRO( I(0), SM(), CM(), T(GRAVE), RM(), END ) : 
                     MACRO_NONE );
     }
     return MACRO_NONE;
